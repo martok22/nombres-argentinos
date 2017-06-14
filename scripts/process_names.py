3755b236eb5bf1b111140ebeb11a95e73f499fd6
Pili Mayora <pili.mayora@gmail.com>
2017-06-14 13:02:42 -0300

    Pegarle a la base in progress

--- scripts/process_names.py
+++ scripts/process_names.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-import sqlalchemy
 from sqlalchemy import create_engine
-# import re
+import re
 import yaml
+import json
 
 with open('../config/mysql.conf', 'r') as f:
     config = yaml.load(f)
@@ -16,19 +16,19 @@ engine = create_engine(
     pool_recycle=3600)
 
 
-# def format_name(name):
-#     """ Function que formatea un nombre para el nombre del json"""
-#     formated_name = re.sub('▒~V~R~V~R~A', 'a', name)
-#     formated_name = re.sub('▒~V~R~I', 'e', formated_name)
-#     formated_name = re.sub('▒~V~R~M', 'i', formated_name)
-#     formated_name = re.sub('▒~V~R~S', 'o', formated_name)
-#     formated_name = re.sub('▒~V~R~Z', 'u', formated_name)
-#     formated_name = re.sub('▒~V~R~Q', 'n', formated_name)
-#     formated_name = re.sub('[^\sa-zA-Z\d]+', ' ', formated_name)
-#     formated_name = re.sub('\s+', '_', formated_name)
-#     formated_name = re.sub('^_+', '', formated_name)
-#     formated_name = re.sub('_+$', '', formated_name)
-#     return formated_name.lower()
+def format_name(name):
+    """ Function que formatea un nombre para el nombre del json"""
+    formated_name = re.sub('▒~V~R~V~R~A', 'a', name)
+    formated_name = re.sub('▒~V~R~I', 'e', formated_name)
+    formated_name = re.sub('▒~V~R~M', 'i', formated_name)
+    formated_name = re.sub('▒~V~R~S', 'o', formated_name)
+    formated_name = re.sub('▒~V~R~Z', 'u', formated_name)
+    formated_name = re.sub('▒~V~R~Q', 'n', formated_name)
+    formated_name = re.sub('[^\sa-zA-Z\d]+', ' ', formated_name)
+    formated_name = re.sub('\s+', '_', formated_name)
+    formated_name = re.sub('^_+', '', formated_name)
+    formated_name = re.sub('_+$', '', formated_name)
+    return formated_name.lower()
 
 
 # Load csv into dataframe
@@ -45,31 +45,29 @@ df = df.groupby(('name', 'year', 'gender')).agg(
     {'quantity': sum, 'percentage': sum}).reset_index()
 
 # Escribir dataframe a base
-# df.to_sql('nombres', engine, flavor='mysql',
-#           if_exists='replace', chunksize=20000)
-# print("Terminamos de escribir nombres a la base")
+df.to_sql('nombres', engine, flavor='mysql', if_exists='replace')
 
 # --- Start calculos de top anuales ---
 
 # Agrupar nombres por anio
 df_by_year = df.groupby('year')
 
-# # Iterar por los años y generar jsons anuales dividos por genero
-# for name, group in df_by_year:
-#     year = format_name(str(name))
-#     top_year = {}
-#     df_by_gender = group.groupby('gender')
-#     for name, group in df_by_gender:
-#         gender = name.lower()
-#         df_year_gender = group.sort_values(by=['quantity'], ascending=False)[
-#             ['name', 'quantity']].head(10)
-#         df_year_gender['name'] = df_year_gender[
-#             'name'].apply(lambda x: format_name(x))
-#         top_year[gender] = df_year_gender.to_dict('records')
+# Iterar por los años y generar jsons anuales dividos por genero
+for name, group in df_by_year:
+    year = format_name(str(name))
+    top_year = {}
+    df_by_gender = group.groupby('gender')
+    for name, group in df_by_gender:
+        gender = name.lower()
+        df_year_gender = group.sort_values(by=['quantity'], ascending=False)[
+            ['name', 'quantity']].head(10)
+        df_year_gender['name'] = df_year_gender[
+            'name'].apply(lambda x: format_name(x))
+        top_year[gender] = df_year_gender.to_dict('records')
 
-#     file_url = '../public/years/' + year + '.json'
-#     with open(file_url, 'w') as fp:
-#         json.dump(top_year, fp)
+    file_url = '../public/years/' + year + '.json'
+    with open(file_url, 'w') as fp:
+        json.dump(top_year, fp)
 
 # Para debug
 # print('--------- termino anios ----------')
@@ -79,25 +77,25 @@ df_by_year = df.groupby('year')
 # Agregar columna para decada
 df['decade'] = df['year'] // 10 * 10
 
-# dfgd = df.groupby(('decade', 'gender', 'name')).agg({'quantity': sum})
-# dfgdd = dfgd.groupby(level=0)
-# for name, group in dfgdd:
-#     decade = format_name(str(name))
-#     top_decade = {}
-#     dfg_gender = group.groupby(level=[0, 1])
-#     for name, group in dfg_gender:
-#         gender = name[1].lower()
-#         gender_group = group.reset_index().sort_values(
-#             by=['quantity'], ascending=False)[['name', 'quantity']].head(10)
-#         gender_group['name'] = gender_group[
-#             'name'].apply(lambda x: format_name(x))
-#         top_decade[gender] = gender_group.to_dict('records')
+dfgd = df.groupby(('decade', 'gender', 'name')).agg({'quantity': sum})
+dfgdd = dfgd.groupby(level=0)
+for name, group in dfgdd:
+    decade = format_name(str(name))
+    top_decade = {}
+    dfg_gender = group.groupby(level=[0, 1])
+    for name, group in dfg_gender:
+        gender = name[1].lower()
+        gender_group = group.reset_index().sort_values(
+            by=['quantity'], ascending=False)[['name', 'quantity']].head(10)
+        gender_group['name'] = gender_group[
+            'name'].apply(lambda x: format_name(x))
+        top_decade[gender] = gender_group.to_dict('records')
 
-#     file_url = '../public/years/decada-' + decade + '.json'
-#     with open(file_url, 'w') as fp:
-#         json.dump(top_decade, fp)
+    file_url = '../public/years/decada-' + decade + '.json'
+    with open(file_url, 'w') as fp:
+        json.dump(top_decade, fp)
 
-# # Para debug
+# Para debug
 # print('---------- termino decadas ------------')
 
 # --- Start procesamiento de nombres
@@ -137,30 +135,19 @@ for index, unique_names in enumerate(split_unique_names):
 
     if index == 0:
         todos_con_todos.to_sql('cruce_nombres_anios',
-                               engine, flavor='mysql', index=False,
-                               if_exists='replace',
-                               dtype={'name': sqlalchemy.NVARCHAR(length=255),
-                                      'gender':
-                                      sqlalchemy.types.NVARCHAR(length=1),
-                                      'year': sqlalchemy.types.Integer,
-                                      'percentage':
-                                      sqlalchemy.types.Float(precision=10,
-                                                             asdecimal=True),
-                                      'quantity': sqlalchemy.types.BigInteger,
-                                      'decade': sqlalchemy.types.Integer})
+                               engine, flavor='mysql', if_exists='replace')
     else:
         todos_con_todos.to_sql('cruce_nombres_anios',
-                               engine, flavor='mysql', index=False,
-                               if_exists='append')
+                               engine, flavor='mysql', if_exists='append')
 
-#     # names_group = todos_con_todos.groupby('name')
+    names_group = todos_con_todos.groupby('name')
 
-#     # Escribir jsons de nombres
-#     # for name, group in names_group:
-#     #     file_url = '../public/names/' + format_name(str(name)) + '.json'
-#     #     print(file_url)
-#     #     list_name = group.reset_index(
-#     #     )[['gender', 'year', 'percentage', 'quantity']].to_dict('records')
+    # Escribir jsons de nombres
+    for name, group in names_group:
+        file_url = '../public/names/' + format_name(str(name)) + '.json'
+        print(file_url)
+        list_name = group.reset_index(
+        )[['gender', 'year', 'percentage', 'quantity']].to_dict('records')
 
-#     #     with open(file_url, 'w') as fp:
-#     #         json.dump(list_name, fp)
+        with open(file_url, 'w') as fp:
+            json.dump(list_name, fp)
